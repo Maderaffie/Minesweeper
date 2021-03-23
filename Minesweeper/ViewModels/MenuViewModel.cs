57f5ad50e290768d523e00fcbba2c04d2f7251f3
Maderaffie <szwonoliwia@gmail.com>
2021-03-23 11:43:50 +0100

    Menu commands created

--- Minesweeper/ViewModels/MenuViewModel.cs
+++ Minesweeper/ViewModels/MenuViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Minesweeper.Commands;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,9 +8,30 @@ namespace Minesweeper.ViewModels
     public class MenuViewModel : BaseViewModel
     {
         private readonly MainWindowViewModel _mainWindowViewModel;
+        public BaseCommand OpenGameViewCommand { get; set; }
+        public BaseCommand OpenSettingsViewCommand { get; set; }
+        public BaseCommand OpenCreditsViewCommand { get; set; }
         public MenuViewModel(MainWindowViewModel mainWindowViewModel)
         {
             _mainWindowViewModel = mainWindowViewModel;
+            OpenCreditsViewCommand = new BaseCommand(OpenCreditsView);
+            OpenGameViewCommand = new BaseCommand(OpenGameView);
+            OpenSettingsViewCommand = new BaseCommand(OpenSettingsView);
+        }
+
+        public void OpenGameView()
+        {
+            _mainWindowViewModel.SelectedViewModel = new GameViewModel(_mainWindowViewModel);
+        }
+
+        public void OpenSettingsView()
+        {
+            _mainWindowViewModel.SelectedViewModel = new GameViewModel(_mainWindowViewModel);
+        }
+
+        public void OpenCreditsView()
+        {
+            _mainWindowViewModel.SelectedViewModel = new GameViewModel(_mainWindowViewModel);
         }
     }
 }
